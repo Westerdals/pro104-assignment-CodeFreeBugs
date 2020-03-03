6cd0da8ed7fe87841619434fc90999908e9f0845
UnstableCodebase <56641204+UnstableCodebase@users.noreply.github.com>
2020-03-03 09:33:13 +0100

    Add files via upload
    Filer vi kan endre på.

---
+++ ourScript.js
@@ -0,0 +1,40 @@
+function renderProductList(){
+    const productList = JSON.parse(window.localStorage.getItem("productList")) || [];
+    const productListEl = document.getElementById("productList");
+    productListEl = "";
+    for(const product of productList){
+        const productEl = document.createElement("div");
+        const{name, price, description} = product;
+        productEl.innerHTML = "<h4>" + name +"</h4>" +
+            "<div>" + description + "</div>" +
+            "<div><small>Price: "+ price + "</small></div>";
+        productListEl.appendChild(productEl);
+
+    }
+}
+function createNewProduct(event){
+    event.preventDefault();
+
+    const name = document.querySelector("[name='name']").value;
+    const price = document.querySelector("[name='price']").value;
+    const description = document.querySelector("[name='description']").value;
+
+    const product = {name, price, description}; 
+
+    
+
+    const productList = JSON-parse(window.localStorage.getItem("productList")) || [];
+    productList.push(product);
+    window.localStorage.setItem("productList", JSON.stringify(productList));
+    renderProductList(); 
+    
+    event.target.reset(); 
+
+
+}
+window.addEventListener("storage",function(event){
+        if(event.key === "productList"){
+            renderProductList();
+        }
+    
+}
