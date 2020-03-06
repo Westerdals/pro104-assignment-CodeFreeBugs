472040ad4da155bd88da1613bc11d954ed5990d9
Andreas Kirkfjell <anki006@student.kristiania.no>
2020-03-06 15:31:48 +0100

    List tasks and assignments work

--- script.js
+++ script.js
@@ -18,14 +18,17 @@
 document.querySelectorAll('.submit')[0].addEventListener('click', (Event) => {
     Event.preventDefault();
     createTeamMember();
-    console.log('submit teamMember');
 });
 
 // Create task after filling inputFields and clicking submit
 document.querySelectorAll('.submit')[1].addEventListener('click', (Event) => {
     Event.preventDefault();
     createTask();
-    console.log('submit new task');
+});
+
+document.querySelectorAll('.submit')[2].addEventListener('click', (Event) => {
+    Event.preventDefault();
+    assignTaskToTeamMember();
 });
 
 //Create teammember
@@ -97,14 +100,26 @@ function createTeamMember() {
 // fill input fields with info (se below)
 // Task involves: name, description, startdate, enddate, deadline
 // Store into localStorage
-const createTask = () => {
-    const inputFields = document.querySelectorAll('.input');
-    const taskArray = [];
+function createTask() {
+    const name = document.querySelector('[name=work]').value;
+    const taskList = JSON.parse(localStorage.getItem('taskList')) ?? [];
 
-    inputFields.forEach(input => {
-        taskArray.push(input.value);
-        localStorage.setItem("taskList", JSON.stringify(taskArray));
-    });
+    const task = {name};
+    this.task = task;
+
+    if (localStorage.length < 1) {
+        taskList.push(task);
+        localStorage.setItem('taskList', JSON.stringify(taskList));
+        listTasks();
+    } else {
+        alreadyRegistered(this.task, taskList);
+    }
+
+    if (localStorage.length > 0 && !alreadyRegistered(this.task, taskList)) {
+        taskList.push(task);
+        localStorage.setItem('taskList', JSON.stringify(taskList));
+        listTasks();
+    }
 }
 
 // List members
@@ -112,7 +127,6 @@ const createTask = () => {
 // loop through and output to div.
 // Should it run on interval?
 const listTeamMembers = () => {
-    const memberList = JSON.parse(localStorage.getItem('teamMemberList'));
     const memberOutputHeader = document.querySelector('#team-members-header');
     const teamMemberList = JSON.parse(localStorage.getItem('teamMemberList'));
     const teamMemberName = document.querySelector('[name=teamMemberName]').value;
@@ -129,20 +143,64 @@ const listTeamMembers = () => {
 // List assignments/task
 // Get info from localStorage.
 // loop through and output to div.
+const listTasks = () => {
+    const taskListHeader = document.querySelector('[name=tasks]');
+    const taskList = JSON.parse(localStorage.getItem('taskList'));
+    const taskName = document.querySelector('[name=work]').value;
 
+    for (const task of taskList) {
+        if (taskName === task.name) {
+            const taskItem = document.createElement('p');
+            taskListHeader.append(taskItem);
+            taskItem.textContent = task.name;
+        }
+    }
+}
 
 // Assign task to teammember
 // Remove task from page and put it under the "teamMember"?
 // Make the teamMember "hold onto the task"
+function assignTaskToTeamMember() {
+    const assignedTaskList = JSON.parse(localStorage.getItem('assignedTaskList')) ?? [];
+    const name = document.querySelector('[name=worker]').value;
+    const task = document.querySelector('[name=work]').value;
 
-// create "case" based on task given to member
-// Set owner from no-one to someone.
+    const assignedTask = {name, task};
+    this.assignedTask = assignedTask;
 
-// list all "cases"
-// List all cases with status?
+    if (localStorage.length < 1) {
+        assignedTaskList.push(assignedTask);
+        localStorage.setItem('taskList', JSON.stringify(assignedTaskList));
+        listAssignedTasks();
+    } else {
+        alreadyRegistered(this.assignedTask, assignedTaskList);
+    }
+
+    if (localStorage.length > 0 && !alreadyRegistered(this.assignedTask, assignedTaskList)) {
+        assignedTaskList.push(assignedTask);
+        localStorage.setItem('assignedTaskList', JSON.stringify(assignedTaskList));
+        listAssignedTasks();
+    }
+}
+
+const listAssignedTasks = () => {
+    const assignedTasksHeader = document.querySelector('[name=assignedTasks]');
+    const assignedTaskList = JSON.parse(localStorage.getItem('assignedTaskList'));
+    const assignedTaskName = document.querySelector('[name=work]').value;
+    const assignedTaskOwner = document.querySelector('[name=worker]').value;
+
+    for (const assignedTask of assignedTaskList) {
+        if (assignedTaskName === assignedTask.task) {
+            
+            const assignedTaskItem = document.createElement('p');
+            assignedTasksHeader.append(assignedTaskItem);
+            assignedTaskItem.textContent = assignedTask.task + " is assigned to " + assignedTaskOwner;
+        }
+    }
+}   
+
 
 // Create HTML content on page load
-
 const generateTeamMemberHeader = () => {
     const teamMemberOutput = document.querySelector('#team-members-div');
     const memberOutputHeader = document.createElement('h2');
@@ -156,7 +214,7 @@ function alreadyRegistered(entity, list) {
     let alreadyRegistered = false;
     for (const item of list) {
         if (entity.name === item.name) {
-            console.error(`The member ${this.member.name} has already been registered`);
+            console.error(`The member ${entity.name} has already been registered`);
             alreadyRegistered = true;
             return alreadyRegistered;
         }
